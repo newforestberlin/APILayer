c2444809351c3bd907c7b218f31e6c02d50f7b35
Ole <ole@oles-mbp.localdomain>
2015-03-15 23:22:45 +0100

    Removed Alamofire binary, added APILayer framework target to support carthage integration

---
+++ APILayerDemo/APILayer/APILayer.h
@@ -0,0 +1,19 @@
+//
+//  APILayer.h
+//  APILayer
+//
+//  Created by Ole on 15.03.15.
+//  Copyright (c) 2015 you & the gang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APILayer.
+FOUNDATION_EXPORT double APILayerVersionNumber;
+
+//! Project version string for APILayer.
+FOUNDATION_EXPORT const unsigned char APILayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APILayer/PublicHeader.h>
+
+
