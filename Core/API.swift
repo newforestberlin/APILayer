f6f106b4121ce1b9bc81dca812f460eda91b1831
Ole <ole@pixelogik.de>
2015-06-25 17:31:53 +0200

    Added token refresh logic

--- Core/API.swift
+++ Core/API.swift
@@ -24,6 +24,22 @@
 import Foundation
 import Alamofire
 
+// Setting the delegate is optional. If set, it can control how the API handles auth token refreshing
+public protocol TokenRefreshDelegate {
+    
+    // For each response the delegate is asked if it implies that token refresh is neeed. Could check for HTTP status for example.
+    func tokenRefreshIsIndicated(byResponse response: NSHTTPURLResponse) -> Bool
+    
+    // Must refresh the token and call the completion block on failure or succcess. Should do a refresh request.
+    // If refresh was successful, the waiting requests are performed in order and everything goes on. If however
+    // refreshing failed, all waiting requests are cancelled and the delegates tokenRefreshHasFailed() method is called,
+    // so that the app can react to that (log out for example).
+    func tokenRefresh(completion: (refreshWasSuccessful: Bool) -> ())
+    
+    // Called if token refresh has failed. In this case all waiting requests are removed and the app should react to that.
+    func tokenRefreshHasFailed()
+}
+
 // Wrapper to make NSURLRequest conform to URLRequestConvertible
 class RequestWrapper: URLRequestConvertible {
     let request: NSURLRequest
@@ -40,15 +56,18 @@ private var API_mapper = ParameterMapper()
 // If this one is set it might return mock paths for router cases, in which case the API does use mock data from local filesystem
 private var API_mocker: MockingProtocol?
 
-// This flag indicates if we are currently refreshing the auth token
-private var API_currentlyRefreshingAuthToken = false
-
 // This queue is used to delay requests when a token refresh is needed. The requests are then performed after the refresh is done.
-private var API_waitingOperations = NSOperationQueue()
+private var API_operations = NSOperationQueue()
+
+// If this is set, we are currently refreshing the token
+private var API_tokenRefreshOperation: NSOperation?
+
+// The optional delegate, that controls token refresh logic
+private var API_tokenRefreshDelegate: TokenRefreshDelegate?
 
 // This class functions as the main interface to the API layer.
 public class API {
-
+    
     // A user of the API is mean to implement a subclass of ParameterWrapper and set it to this property
     public class var parameterMapper: ParameterMapper {
         get {
@@ -69,12 +88,22 @@ public class API {
         }
     }
     
-    // MARK: Private request methods
+    // // The optional delegate, that controls token refresh logic
+    public class var tokenRefreshDelegate: TokenRefreshDelegate? {
+        get {
+            return API_tokenRefreshDelegate
+        }
+        set {
+            API_tokenRefreshDelegate = newValue
+        }
+    }
+    
+    // MARK: Request creation from routers
 
     private class func createRequest(forRouter router: RouterProtocol) -> Request {
         
         // Make sure the operation queue is sequential
-        API_waitingOperations.maxConcurrentOperationCount = 1
+        API_operations.maxConcurrentOperationCount = 1
         
         // Get base URL
         var URL: NSURL?
@@ -103,8 +132,70 @@ public class API {
         return Alamofire.request(RequestWrapper(request: requestTuple.0))
     }
     
+    // MARK: Request performing 
+    
+    private class func performRouter<T: ResponseObjectSerializable>(router: RouterProtocol, complete: (T?, NSHTTPURLResponse?, NSError?) -> ()) {
+        
+        // Do the actual request
+        var request = API.createRequest(forRouter: router)
+        
+        request.responseObject { (_, response: NSHTTPURLResponse?, result: T?, error) in
+            
+            if let response = response, let tokenRefreshDelegate = self.tokenRefreshDelegate {
+                
+                if tokenRefreshDelegate.tokenRefreshIsIndicated(byResponse: response) {
+
+                    // Create the token refresh operation
+                    API_tokenRefreshOperation = TokenRefreshOperation(tokenRefreshDelegate: tokenRefreshDelegate, completion: { (refreshWasSuccessful) -> () in
+                        
+                        if refreshWasSuccessful == false {
+                            // Refreshing failed, let the delegate now
+                            tokenRefreshDelegate.tokenRefreshHasFailed()
+                            // And cancel all
+                            API_operations.cancelAllOperations()
+                        }
+                        
+                        // Reset refresh operation
+                        API_tokenRefreshOperation = nil
+                    })
+                    
+                    // Enqueue the token refresh operation
+                    API_operations.addOperation(API_tokenRefreshOperation!)
+                    
+                    // Enqueue the router, so that after the token refresh it is redone
+                    self.enqueueRouter(router, complete: complete)
+                    
+                    // Do not call the complete block yet
+                    return
+                }
+            }
+            
+            // No refresh needed, call completion block
+            complete(result, response, error)
+        }
+        
+    }
+    
+    // MARK: Request enqueueing
+    
+    private class func enqueueRouter<T: ResponseObjectSerializable>(router: RouterProtocol, complete: (T?, NSHTTPURLResponse?, NSError?) -> ()) {
+
+        var blockOperation = NSBlockOperation(block: {
+            self.performRouter(router, complete: complete)
+        })
+        
+        if let tokenRefreshOperation = API_tokenRefreshOperation {
+            blockOperation.addDependency(tokenRefreshOperation)
+        }
+        
+        API_operations.addOperation(blockOperation)
+    }
+    
+    // MARK: Private request method
+    
     private class func completeRequest<T: ResponseObjectSerializable>(router: RouterProtocol, complete: (T?, NSHTTPURLResponse?, NSError?) -> ()) -> Request {
 
+        // TODO: This request is not used !
         var request = API.createRequest(forRouter: router)
         
         if let mocker = API_mocker, let path = mocker.path(forRouter: router) {
@@ -113,15 +204,26 @@ public class API {
             })
         }
         else {
-            request.responseObject { (_, response: NSHTTPURLResponse?, result: T?, error) in
-                complete(result, response, error)
-            }
+            enqueueRouter(router, complete: complete)
         }
         
         return request
     }
     
     // MARK: Public request methods
+    
+    public class func tokenRefresh<T: ResponseObjectSerializable>(router: RouterProtocol, complete: (T?, NSHTTPURLResponse?, NSError?) -> ()) -> Request {
+        
+        var request = API.createRequest(forRouter: router)
+        
+        request.responseObject { (_, response: NSHTTPURLResponse?, result: T?, error) in
+            // No refresh needed, call completion block
+            
+            complete(result, response, error)
+        }
+        
+        return request
+    }
     
     // Performs request with the specified Router. Completion block is called in case of success / failure later on.
     public class func request<T: ResponseObjectSerializable>(router: RouterProtocol, complete: (T?, NSError?) -> ()) -> Request {
